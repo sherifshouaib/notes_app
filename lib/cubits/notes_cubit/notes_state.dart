c3124d44cc826df73545bcfab373ef73a7c9a1ce
sherifshouaib <sherifshouaib22@gmail.com>
2024-04-07 17:25:45 +0200

    create notes state

---
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/models/note_model.dart';
+
+@immutable
+abstract class NotesState {}
+
+class NotesInitial extends NotesState {}
+
+class NotesLoading extends NotesState {}
+
+class NotesSuccess extends NotesState {
+  final List<NoteModel> notes;
+
+  NotesSuccess(this.notes);
+}
+
+class NotesFailure extends NotesState {
+  final String errMessage;
+
+  NotesFailure(this.errMessage);
+}
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -11,9 +11,10 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
-            shape: RoundedRectangleBorder(
-             borderRadius: BorderRadius.circular(16),
-            ),
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
               context: context,
               builder: (context) {
                 return const AddNoteBottomSheet();
